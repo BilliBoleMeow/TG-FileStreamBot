ff7f4f455130ff8a61caea946d8bdb84c52634f3
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2023-04-16 17:15:00 +0530

    typo

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.enums.parse_mode import ParseMode
 from WebStreamer.vars import Var
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.command(["start", "help"]) & filters.private
+@StreamBot.on_message(filters.command(["start", "help"]) & filters.private)
 async def start(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply(
