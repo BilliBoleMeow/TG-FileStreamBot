4355b08a2b6ec8c7c44e9e72e7ddd6f0f617c101
BilliBoleMeow <88665437+BilliBoleMeow@users.noreply.github.com>
2023-04-16 17:14:09 +0530

    readd allowed users var related things

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -5,10 +5,17 @@ from pyrogram import filters
 from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
 from pyrogram.enums.parse_mode import ParseMode
+from WebStreamer.vars import Var
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-@StreamBot.on_message(filters.command(["start", "help"]))
+@StreamBot.on_message(filters.command(["start", "help"]) & filters.private
 async def start(_, m: Message):
+    if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
+        return await m.reply(
+            "You are not in the allowed list of users who can use me. \
+            Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
+            disable_web_page_preview=True, quote=True
+        )
     await m.reply(
         text="""
 <b>A Simple Telegram File To Link Generation Bot.</b>\n.                           <b>Maintained By 👇</b>""",
